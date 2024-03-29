6e71cb4109d281a07dfa97b136cb4e37824510d2
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2024-03-29 15:08:56 +0100

    updated

--- resources/js/http.js
+++ resources/js/http.js
@@ -14,9 +14,6 @@ http.interceptors.response.use( (response) => {
             clearValidationErrors(domElement)
             handleValidationErrors(error.response.data.errors, domElement)  
         }
-        if (error.response.status === 401) {
-            location.href = '/'  
-        }
     return Promise.reject(error)
 })
 
