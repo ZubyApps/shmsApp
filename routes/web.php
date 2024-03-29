4ca611f9981f7f027c1e68676bca24caf748578b
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2024-03-29 07:13:55 +0100

    route adjustments

--- routes/web.php
+++ routes/web.php
@@ -60,15 +60,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    // Route::get('users', [RegisteredUserController::class, 'create'])->name('users');
-    // Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
-    // Route::get('/billing', [BillingController::class, 'index'])->name('Billing');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+Route::middleware('auth')->group(function () {    
     Route::middleware('strict')->group(function () {
         Route::get('/admin', [AdminController::class, 'index'])->name('Admin');
         Route::get('/admin/settings', [AdminController::class, 'settings'])->name('Settings');
