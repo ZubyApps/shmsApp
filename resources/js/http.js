8ceb0aebb61f5aa90057709b2d43e39b9ad5afa5
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2024-03-29 15:56:43 +0100

    restored interceptors

--- resources/js/http.js
+++ resources/js/http.js
@@ -11,10 +11,10 @@ http.interceptors.response.use( (response) => {
     return response
 }, (error) => {
     const domElement = error.config.html
-        // if (error.response.status === 422) {
-        //     clearValidationErrors(domElement)
-        //     handleValidationErrors(error.response.data.errors, domElement)  
-        // }
+        if (error.response.status === 422) {
+            clearValidationErrors(domElement)
+            handleValidationErrors(error.response.data.errors, domElement)  
+        }
     return Promise.reject(error)
 })
 
