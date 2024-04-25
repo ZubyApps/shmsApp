4da9ce9901f3420e381fbc342196fefa4d34924c
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2024-04-25 22:15:26 +0100

    division by zero fix

--- app/Services/PayPercentageService.php
+++ app/Services/PayPercentageService.php
@@ -15,16 +15,16 @@ Class PayPercentageService
 
     public function individual_Family(Visit $visit): int|float|null
     {
-        return $visit->totalHmsBills() ? round((float)($visit->totalPayments() / ($visit->totalHmsBills() - $visit->discount)) * 100, 2) : null;
+        return $visit->totalHmsBills() && $visit->totalHmsBills() != $visit->discount  ? round((float)($visit->totalPayments() / ($visit->totalHmsBills() - $visit->discount)) * 100, 2) : null;
     }
 
     public function hmo_Retainership(Visit $visit): int|float|null
     {
-        return $visit->totalHmsBills() ? round((float)($visit->totalApprovedBills() / ($visit->totalHmsBills() - $visit->discount)) * 100) : null;
+        return $visit->totalHmsBills() && $visit->totalHmsBills() != $visit->discount ? round((float)($visit->totalApprovedBills() / ($visit->totalHmsBills() - $visit->discount)) * 100) : null;
     }
 
     public function nhis(Visit $visit): int|float|null
     {
-        return $visit->totalHmsBills() ? round((float)($visit->totalPayments() / ($visit->totalNhisBills() - $visit->discount)) * 100, 2) : null;
+        return $visit->totalHmsBills() && $visit->totalNhisBills() != $visit->discount ? round((float)($visit->totalPayments() / ($visit->totalNhisBills() - $visit->discount)) * 100, 2) : null;
     }
 }
