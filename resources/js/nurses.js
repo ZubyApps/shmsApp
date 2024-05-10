be519ad734bbb78994e0f7b3ae41b0b06c47a708
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2024-05-10 10:58:30 +0100

    retain table state

--- resources/js/nurses.js
+++ resources/js/nurses.js
@@ -390,8 +390,8 @@ window.addEventListener('DOMContentLoaded', function () {
                         if (response.status >= 200 || response.status <= 300) {
                             markDoneBtn.removeAttribute('disabled') 
                             inPatientsVisitTable.draw(false); upcomingMedicationsTable.draw(); upcomingNursingChartsTable.draw()
-                            outPatientsVisitTable ? outPatientsVisitTable.draw() : ''
-                            ancPatientsVisitTable ? ancPatientsVisitTable.draw() : ''
+                            outPatientsVisitTable ? outPatientsVisitTable.draw(false) : ''
+                            ancPatientsVisitTable ? ancPatientsVisitTable.draw(false) : ''
                         }
                       })
                     .catch((error) => {
