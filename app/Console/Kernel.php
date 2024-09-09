7a21fc7fe12fba3883e62636845a3ad3c7cad22b
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2024-09-09 01:42:53 +0100

    removed test logs and test timings

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,6 @@ use App\Notifications\MedicationNotifier;
 use App\Services\ChurchPlusSmsService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,8 +25,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(new NursesNightShift)->timezone('Africa/Lagos')->dailyAt('19:30');
         $schedule->call(new Remind)->timezone('Africa/Lagos')->twiceDaily(9, 15);
         $schedule->call(new CleanUpTables)->timezone('Africa/Lagos')->lastDayOfMonth('23:59');
-        $schedule->call(new MedicationsForSms(new MedicationNotifier(new ChurchPlusSmsService())))->timezone('Africa/Lagos');//->between('08:00', '20:00');
-        Log::info("schedule ran");
+        $schedule->call(new MedicationsForSms(new MedicationNotifier(new ChurchPlusSmsService())))->timezone('Africa/Lagos')->between('08:00', '20:00');
     }
 
     /**
--- app/Services/ChurchPlusSmsService.php
+++ app/Services/ChurchPlusSmsService.php
@@ -33,8 +33,8 @@ class ChurchPlusSmsService
         $client = new Client();
 
         $completeUrl = $this->baseUrl.'recipients='.$recipients.'&message='.$message.'&subject='.$subject.'&cid='.$this->tenantId;
-        
-        Log::info("", ["the url" => $completeUrl]);
+
+        // Log::info("", ["the url" => $completeUrl]);
 
         $response = $client->request('POST', $completeUrl, []);
 
