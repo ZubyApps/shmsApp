844a3e1340fe815809bc7130349a641acb8d542b
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2026-01-02 10:41:14 +0100

    Events bug fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,14 +2,19 @@
 
 namespace App\Providers;
 
+use App\Events\BulkPrescriptionsCreated;
 use App\Events\CapitationPaymentCreated;
+use App\Events\CapitationPaymentDeleted;
 use App\Events\PaymentCreated;
 use App\Events\PaymentDestroyed;
 use App\Events\PrescriptionBilled;
 use App\Events\PrescriptionCreated;
+use App\Events\PrescriptionDeleted;
 use App\Events\PrescriptionTreated;
+use App\Listeners\UpdateBulkPrescriptionCreated;
 use App\Listeners\UpdateCapitationPayment;
 use App\Listeners\UpdateCapitationPaymentCreated;
+use App\Listeners\UpdateCapitationPaymentDeleted;
 use App\Listeners\UpdateHmoTreated;
 use App\Listeners\UpdateVisitBilling;
 use Illuminate\Support\Facades\Event;
@@ -18,6 +23,7 @@ use App\Listeners\UpdateMortuaryBilling;
 use App\Listeners\UpdatePaymentCreated;
 use App\Listeners\UpdatePaymentDestroyed;
 use App\Listeners\UpdatePrescriptionBilled;
+use App\Listeners\UpdatePrescriptionDeleted;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -42,6 +48,10 @@ class EventServiceProvider extends ServiceProvider
             UpdatePrescriptionBilled::class
         ],
 
+        PrescriptionDeleted::class => [
+            UpdatePrescriptionDeleted::class
+        ],
+
         PaymentCreated::class => [
             UpdatePaymentCreated::class,
         ],
@@ -52,6 +62,14 @@ class EventServiceProvider extends ServiceProvider
 
         CapitationPaymentCreated::class => [
             UpdateCapitationPaymentCreated::class
+        ],
+
+        CapitationPaymentDeleted::class => [
+            UpdateCapitationPaymentDeleted::class
+        ],
+
+        BulkPrescriptionsCreated::class => [
+            UpdateBulkPrescriptionCreated::class
         ]
     ];
 
