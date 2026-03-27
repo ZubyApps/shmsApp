f14146295ecc5f238abd0c257729fa50cba4c049
ZubyApps <109441625+ZubyApps@users.noreply.github.com>
2026-03-27 10:21:26 +0100

    Bug fix

--- app/Http/Resources/SmsDetailsResource.php
+++ app/Http/Resources/SmsDetailsResource.php
@@ -17,11 +17,13 @@ class SmsDetailsResource extends JsonResource
     {
         // $amountInWords  = new NumberFormatter();
         // $amountInWords->setTextAttribute(NumberFormatter::DEFAULT_RULESET, "%spellout-numbering-verbose");
-        $outstanding    = $this->visit->total_hms_bill - $this->visit->discount - $this->visit->total_paid;
+        $isNhis = $this->visit->sponsor->category_name == 'NHIS';
+        $visit  = $this->visit;
+        $outstanding    = ($isNhis ? $visit->total_nhis_bill : $visit->total_hms_bill) - $visit->discount - $visit->total_paid;
         return [
             'id'            => $this->id,
-            'phone'         => $this->visit->patient->phone,
-            'smsDetails'   => 'Dear ' . $this->visit->patient->first_name . ' ' . $this->visit->patient->last_name . ', pls be reminded of your hospital bill (N' .number_format($outstanding). ') incurred on ' . (new Carbon($this->visit->created_at))->format('d/m/Y') . '. Kindly send to Monie Point Nzube Okoye 5496896686 or visit the hospital',
+            'phone'         => $visit->patient->phone,
+            'smsDetails'   => 'Dear ' . $visit->patient->first_name . ' ' . $visit->patient->last_name . ', pls be reminded of your hospital bill (N' .number_format($outstanding). ') incurred on ' . (new Carbon($visit->created_at))->format('d/m/Y') . '. Kindly send to Monie Point Nzube Okoye 5496896686 or visit the hospital',
         ];
     }
 }
